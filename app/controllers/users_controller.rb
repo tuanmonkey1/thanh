52b28f4a132b426e06f948d84e974818c01ee211
Your Name <your.email@example.com>
2020-11-05 10:33:02 +0700

    signup

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,28 @@
+class UsersController < ApplicationController
+	def show
+		@user = User.find(params[:id])
+	end
+
+	def new
+		@user = User.new
+	end
+
+	def create
+		def create
+			@user = User.new(user_params)
+			if @user.save
+				log_in @user
+			redirect_to @user
+			# Handle a successful save.
+			else
+				render 'new'
+			end
+		end
+	end
+	private
+	def user_params
+		params.require(:user).permit(:name, :email, :password,
+									   :password_confirmation)
+	
+	end
+end
