7e39e25ac55129467c9ace16185aeb1ee5e6573c
Your Name <your.email@example.com>
2020-11-05 14:50:30 +0700

    signup

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -5,9 +5,10 @@ class SessionsController < ApplicationController
  
   def create
 	user = User.find_by(email: params[:session][:email].downcase)
-	if user&.authenticate(params[:session][:password])
+	if user && user.authenticate(params[:session][:password])
 		log_in user
-		redirect_to user
+		params[:session][:remember_me] == '1' ? remember(user) : forget(user)
+		redirect_back_or user
 	else
 		flash.now[:danger] = 'Invalid email/password combination'
 		render 'new'
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+	before_action :logged_in_user, only: [:index, :edit, :update]
+	before_action :correct_user, only: [:edit, :update]
 	def show
 		@user = User.find(params[:id])
 	end
@@ -7,22 +9,54 @@ class UsersController < ApplicationController
 		@user = User.new
 	end
 
+	def index
+		@users = User.paginate(page: params[:page])
+	end
+
 	def create
-		def create
-			@user = User.new(user_params)
-			if @user.save
-				log_in @user
-			redirect_to @user
+		@user = User.new(user_params)
+		if @user.save
+			log_in @user
+			redirect_to root_url
 			# Handle a successful save.
-			else
-				render 'new'
-			end
+		else
+			render 'new'
 		end
 	end
+	
+	def edit
+		@user = User.find(params[:id])
+	end
+	
+	def update
+		@user = User.find(params[:id])
+		if @user.update(user_params)
+			flash[:success] = "Profile updated"
+			redirect_to @user
+		else
+			render 'edit'
+		end
+	end
+
+
 	private
 	def user_params
 		params.require(:user).permit(:name, :email, :password,
 									   :password_confirmation)
 	
+	end
+
+	# Before filters
+	# Confirms a logged-in user.
+	def logged_in_user
+		unless logged_in?
+			flash[:danger] = "Please log in."
+			redirect_to login_url
+		end
+	end
+	# Confirms the correct user.
+	def correct_user
+		@user = User.find(params[:id])
+		redirect_to(root_url) unless current_user?(@user)
 	end
 end
